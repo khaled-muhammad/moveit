766545e8737ae894a810eb9d04e020763d690fc6
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-27 02:14:13 +0300

    Separated QRCode display too into a separate component

---
+++ src/components/QRCodeDisplay.jsx
@@ -0,0 +1,17 @@
+import QRCode from "react-qr-code";
+
+const QRCodeDisplay = ({ session, size = 300, className = "" }) => {
+  if (!session) return null;
+  
+  return (
+    <QRCode
+      value={JSON.stringify(session)}
+      fgColor="#fff"
+      bgColor="#4C319CFF"
+      size={size}
+      className={`rounded-md drop-shadow-2xl drop-shadow-[#7f5af0a1] ${className}`}
+    />
+  );
+};
+
+export default QRCodeDisplay;
