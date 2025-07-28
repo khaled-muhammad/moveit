be71c88f7465c4dfb543f6ff7bb7b1351d5d0b48
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-28 21:33:18 +0300

    Make it appear above anything with no cursor

--- src/components/QRCodeDisplay.jsx
+++ src/components/QRCodeDisplay.jsx
@@ -9,7 +9,7 @@ const QRCodeDisplay = ({ session, size = 300, className = "" }) => {
       fgColor="#fff"
       bgColor="#4C319CFF"
       size={size}
-      className={`rounded-md drop-shadow-2xl drop-shadow-[#7f5af0a1] ${className}`}
+      className={`rounded-md drop-shadow-2xl drop-shadow-[#7f5af0a1] cursor-none relative z-50 ${className}`}
     />
   );
 };
