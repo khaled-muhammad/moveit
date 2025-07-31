c987e5f2f93e032d4194239a5bad4c4bebe833f3
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-31 06:13:31 +0300

    Refactored routing and providers to support react router dom

---
+++ src/Layout.jsx
@@ -0,0 +1,69 @@
+import { NavLink, Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Logo from "./components/Logo";
+import { FiGrid, FiHome } from "react-icons/fi";
+import { useEffect, useState } from "react";
+import { useSession } from "./components/SessionProvider";
+import { WebSocketProvider } from "./components/WebSocketProvider";
+
+const Layout = () => {
+  const [isMobile, setIsMobile] = useState(true);
+  const { session } = useSession();
+
+  useEffect(() => {
+    const checkDevice = () => {
+      setIsMobile(window.innerWidth <= 1024);
+    };
+
+    checkDevice();
+    window.addEventListener("resize", checkDevice);
+
+    return () => {
+      window.removeEventListener("resize", checkDevice);
+    };
+  }, []);
+
+  return (
+    <WebSocketProvider session={session}>
+      <div className="min-h-[100%] flex flex-col justify-between items-center gap-5">
+        <Logo className="text-4xl z-[91474836489999999]" />
+        <Outlet />
+        {isMobile && (
+          <div className="nav-bar py-5 border-t-3 border-t-[#7F5AF0] w-full">
+            <ul className="w-full flex justify-around">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `flex gap-1 flex-col items-center ${
+                      isActive ? "text-[#7F5AF0]" : ""
+                    }`
+                  }
+                >
+                  <FiHome size={28} />
+                  <span>Home</span>
+                </NavLink>
+              </li>
+              <li className="flex gap-1 flex-col items-center">
+                <NavLink
+                  to="/space"
+                  className={({ isActive }) =>
+                    `flex gap-1 flex-col items-center ${
+                      isActive ? "text-[#7F5AF0]" : ""
+                    }`
+                  }
+                >
+                  <FiGrid size={28} />
+                  <span>Space</span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+      <Footer />
+    </WebSocketProvider>
+  );
+};
+
+export default Layout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Toaster } from 'react-hot-toast';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout.jsx";
+import SpacePage from "./routes/SpacePage.jsx";
+import { SessionProvider } from "./components/SessionProvider";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   //<StrictMode>
-  <>
-    <App />
-    <Toaster />
-  </>
+  <SessionProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="space" element={<SpacePage />} />
+        </Route>
+      </Routes>
+      <Toaster />
+    </BrowserRouter>
+  </SessionProvider>
+
   //</StrictMode>,
-)
+);
