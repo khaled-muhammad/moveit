3dcd4327d21a3d36c615f08711616cc5961b6229
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-31 06:38:48 +0300

    Added update to layout to support mobile navbar and many cleanouts and updates according to new providers

--- src/routes/MobilePage.jsx
+++ src/routes/MobilePage.jsx
@@ -4,36 +4,16 @@ import toast from "react-hot-toast";
 import { useSession } from "../components/SessionProvider";
 import { useWebSocketContext } from "../components/WebSocketProvider";
 import Logo from "../components/Logo";
-import { FiCopy, FiUpload } from "react-icons/fi";
+import { FiCopy, FiGrid, FiHome, FiUpload } from "react-icons/fi";
 import UploadButton from "../components/UploadBtn";
 import Footer from "../components/Footer";
+import { uploadToUguu } from "../utils";
 
-import axios from 'axios';
-import { api } from "../consts";
-
-const uploadToUguu = async (file) => {
-  const formData = new FormData();
-  formData.append('file', file);
-
-  try {
-    const response = await api.post('upload/', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
-
-    console.log('Uploaded File Info:', response.data);
-    return response.data;
-  } catch (error) {
-    console.log('Upload failed:', error);
-    return null;
-  }
-};
 
 const MobilePage = () => {
   const [scanning, setScanning] = useState(false);
   const { session, setSession } = useSession();
-  const { isConnected, lastJsonMessage, shareClipBoard, sharedClipboards } = useWebSocketContext();
+  const { isConnected, lastJsonMessage, shareClipBoard, sharedClipboards, setShouldConnect } = useWebSocketContext();
   const queryParams = new URLSearchParams(window.location.search);
   const queryBeamId = queryParams.get('beam_id');
 
@@ -44,6 +24,7 @@ const MobilePage = () => {
         beam_id: queryBeamId,
         beam_key: null
       })
+      setShouldConnect('auto')
       const url = new URL(window.location.href)
       url.searchParams.delete('beam_id')
       window.history.replaceState({}, '', url)
@@ -72,61 +53,59 @@ const MobilePage = () => {
 
   return (
     <>
-      <section className="flex flex-col justify-center items-center min-h-screen gap-5">
-        <Logo className="text-4xl z-[91474836489999999]" />
+        <div className="flex flex-1 flex-col justify-center items-center gap-5">
+            <h1 className="text-xl font-semibold text-center mx-5">{session? "Start Sharing" : <>Open <a className="text-[#7F5AF0]" href={window.location.origin}>{window.location.host}</a> on your PC/Laptop to start beaming</>}</h1>
 
-        <h1 className="text-xl font-semibold text-center mx-5">{session? "Start Sharing" : <>Open <a className="text-[#7F5AF0]" href={window.location.origin}>{window.location.host}</a> on your PC/Laptop to start beaming</>}</h1>
+            {!session && (
+              <button
+                className="brain-boom-btn"
+                onClick={handleStartScanning}
+              >
+                Scan QR
+              </button>
+            )}
+            {scanning && (
+              <Scanner
+                onScan={(result) => {
+                  setScanning(false)
+                  setSession({
+                    beam_id: result[0].rawValue.split('?beam_id=')[1],
+                  beam_key: null,
+                  })
+                  setShouldConnect('auto')
+                }}
+                sound={false}
+              />
+            )}
+            {session && <div className="flex gap-5 flex-wrap">
+              <button className="brain-boom-btn" onClick={() => {
+                navigator.clipboard.readText().then((clipboardContent) => {
+                  if (!clipboardContent) {
+                    toast("Your clipboard is empty!")
+                    return;
+                  }
+                  if (sharedClipboards.filter((cb) => cb.content == clipboardContent).length == 0) {
+                    shareClipBoard(clipboardContent)
+                  }
+                })
+              }}><FiCopy /> Copy</button>
+              <UploadButton selected={(files) => {
+                files.forEach((f) => {
+                  const type = f.type.split('/')[0];
 
-        {!session && (
-          <button
-            className="brain-boom-btn"
-            onClick={handleStartScanning}
-          >
-            Scan QR
-          </button>
-        )}
-        {scanning && (
-          <Scanner
-            onScan={(result) => {
-              setScanning(false)
-              setSession({
-                beam_id: result[0].rawValue.split('?beam_id=')[1],
-              beam_key: null,
-              });
-            }}
-            sound={false}
-          />
-        )}
-        {session && <div className="flex gap-5 flex-wrap">
-          <button className="brain-boom-btn" onClick={() => {
-            navigator.clipboard.readText().then((clipboardContent) => {
-              if (!clipboardContent) {
-                toast("Your clipboard is empty!")
-                return;
-              }
-              if (sharedClipboards.filter((cb) => cb.content == clipboardContent).length == 0) {
-                shareClipBoard(clipboardContent)
-              }
-            })
-          }}><FiCopy /> Copy</button>
-          <UploadButton selected={(files) => {
-            files.forEach((f) => {
-              const type = f.type.split('/')[0];
-
-              toast(`Uploading ${f.name}`)
-              uploadToUguu(f).then((directLink) => {
-                if (!directLink) {
-                  toast.error("Failed to upload!")
-                  return;
-                }
-                toast(`Uploaded "${f.name}" Successfully!`)
-                shareClipBoard(directLink, type)
-              })
-            })
-          }} />
-        </div>}
-      </section>
-      <Footer />
+                  toast(`Uploading ${f.name}`)
+                  uploadToUguu(f).then((directLink) => {
+                    if (!directLink) {
+                      toast.error("Failed to upload!")
+                      return;
+                    }
+                    toast(`Uploaded "${f.name}" Successfully!`)
+                    shareClipBoard(directLink, type)
+                  })
+                })
+              }} />
+            </div>}
+        </div>
     </>
   );
 };
