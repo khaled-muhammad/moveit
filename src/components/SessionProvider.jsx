68eb20b1111d81336575cc05d90e1312e767fa4c
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-07-31 06:19:31 +0300

    Allowed custom new session creation

--- src/components/SessionProvider.jsx
+++ src/components/SessionProvider.jsx
@@ -20,27 +20,32 @@ export const SessionProvider = ({ children }) => {
       if (savedBeamSession != null) {
         setSession(JSON.parse(savedBeamSession));
       } else {
-        api
-          .post("beams/create/")
-          .then((response) => {
-            console.log("Beam created:", response.data);
-            const expiresAt = new Date(
-              Date.now() + 24 * 60 * 60 * 1000
-            ).toISOString();
-            const newSession = { ...response.data, expiresAt: expiresAt };
-            setSession(newSession);
-            localStorage.setItem("session", JSON.stringify(newSession));
-          })
-          .catch((error) => {
-            console.error("Error creating beam:", error);
-          });
+        newSession();
       }
     }
   }, []);
 
+  const newSession = () => {
+      api
+    .post("beams/create/")
+    .then((response) => {
+      console.log("Beam created:", response.data);
+      const expiresAt = new Date(
+        Date.now() + 24 * 60 * 60 * 1000
+      ).toISOString();
+      const newSession = { ...response.data, expiresAt: expiresAt };
+      setSession(newSession);
+      localStorage.setItem("session", JSON.stringify(newSession));
+    })
+    .catch((error) => {
+      console.error("Error creating beam:", error);
+    });
+  }
+
   const value = {
     session,
     setSession,
+    newSession,
   };
 
   return (
