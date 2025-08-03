e5be4bf7d75c96f8d1a822d1b5858ec7149b8b83
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-08-03 23:34:29 +0300

    Wrapped by Auth Provider

--- src/main.jsx
+++ src/main.jsx
@@ -10,22 +10,25 @@ import { SessionProvider } from "./components/SessionProvider";
 import SpaceMenuPage from "./routes/SpaceMenuPage.jsx";
 import LoginPage from "./routes/LoginPage.jsx";
 import RegisterPage from "./routes/RegisterPage.jsx";
+import { AuthProvider } from './contexts/AuthContext.js';
 
 createRoot(document.getElementById("root")).render(
   //<StrictMode>
-  <SessionProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={<Layout />}>
-          <Route index element={<App />} />
-          <Route path="space" element={<SpacePage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
-        </Route>
-      </Routes>
-      <Toaster position="bottom-center" />
-    </BrowserRouter>
-  </SessionProvider>
+  <AuthProvider>
+    <SessionProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Layout />}>
+            <Route index element={<App />} />
+            <Route path="space" element={<SpacePage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterPage />} />
+          </Route>
+        </Routes>
+        <Toaster position="bottom-center" />
+      </BrowserRouter>
+    </SessionProvider>
+  </AuthProvider>
 
   //</StrictMode>,
 );
