9eaa31aa1e7dc56a322d8936093b3f1d6e5c1be1
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-08-05 19:25:05 +0300

    Created a good looking admin view for notes.

--- moveit_backend/note/admin.py
+++ moveit_backend/note/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Note
 
-# Register your models here.
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'title', 'note_type',
+                    'created_at', 'updated_at')
+    list_filter = ('note_type', 'created_at', 'updated_at', 'user')
+    search_fields = ('title', 'content', 'user__username', 'user__email')
+    readonly_fields = ('id', 'created_at', 'updated_at')
+    ordering = ('-updated_at',)
+
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('id', 'user', 'title', 'note_type')
+        }),
+        ('Content', {
+            'fields': ('content', 'json_content'),
+            'classes': ('collapse',)
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('user')
