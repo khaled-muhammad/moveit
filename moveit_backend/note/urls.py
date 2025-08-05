aab19e76f73c8ac7e12247d4dbbe85b1d0d23cdc
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-08-05 19:24:49 +0300

    Created Simple notes urls

---
+++ moveit_backend/note/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import NoteViewSet, NoteDetailView
+
+router = DefaultRouter()
+router.register(r'notes', NoteViewSet, basename='note')
+router.register(r'note-detail', NoteDetailView, basename='note-detail')
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
