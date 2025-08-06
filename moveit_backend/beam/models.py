edcdfd8089a1dbeb4b4463b4430bde1cff743083
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-08-06 09:37:20 +0300

    Added beam name in Beam model

--- moveit_backend/beam/models.py
+++ moveit_backend/beam/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Beam(models.Model):
-    beam_id  = models.CharField(max_length=255, unique=True)
-    beam_key = models.TextField()
-
+    beam_id    = models.CharField(max_length=255, unique=True)
+    beam_key   = models.TextField()
+    beam_name  = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
