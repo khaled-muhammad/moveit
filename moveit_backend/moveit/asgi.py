d557fbedf3fe84c983bf5f6577225f042f74bc17
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-08-07 03:00:40 +0300

    Fix production issue

--- moveit_backend/moveit/asgi.py
+++ moveit_backend/moveit/asgi.py
@@ -4,12 +4,13 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from my_auth.middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moveit.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+from my_auth.middleware import JWTAuthMiddleware
 
 from beam.routing import websocket_urlpatterns
 
