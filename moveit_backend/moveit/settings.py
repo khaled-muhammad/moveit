a576ee654b084ee7b2ddf27a3c666f18bb9f0487
Khaled Muhammad <khaledmuhmmed99@gmail.com>
2025-08-19 01:49:44 +0300

    allow multi-domains in frontend

--- moveit_backend/moveit/settings.py
+++ moveit_backend/moveit/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['*'] if DEBUG else [os.environ.get("FRONTEND_ENDPOINT")]
+ALLOWED_HOSTS = ['*'] if DEBUG else os.environ.get("FRONTEND_ENDPOINT").split(',')
 
 
 # Application definition
